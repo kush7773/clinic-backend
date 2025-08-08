4b25f067e75e4301dad7a6c89ca448b74d459f55
KUSH <tokenadmin@KUSH-MACBOOK-PRO.local>
2025-08-09 03:01:50 +0530

    Configure CORS for production

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Allows the frontend to communicate with the backend
+
+  // More specific CORS configuration for production
+  app.enableCors({
+    origin: 'https://clinic-frontend-bay.vercel.app/', // <-- PASTE YOUR VERCEL URL HERE
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
